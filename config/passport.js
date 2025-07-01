fd411a6e197dfa78a2a6fe2f2123e10c8fe2e9d1
pini91 <brenda.loncaric@gmail.com>
2025-07-01 13:27:43 -0400

    Fixed the passaport

--- config/passport.js
+++ config/passport.js
@@ -3,31 +3,77 @@ const mongoose = require('mongoose')
 const User = require('../models/User')
 
 module.exports = function (passport) {
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email: email.toLowerCase() }, (err, user) => {
-      if (err) { return done(err) }
-      if (!user) {
-        return done(null, false, { msg: `Email ${email} not found.` })
-      }
-      if (!user.password) {
-        return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' })
-      }
-      user.comparePassword(password, (err, isMatch) => {
-        if (err) { return done(err) }
-        if (isMatch) {
-          return done(null, user)
+  passport.use(
+    new LocalStrategy({ usernameField: "email" },async (email, password, done) => {
+
+      const user = await User.findOne({ email: email.toLowerCase() })//, (err, user) => {
+        // if (err) {
+        //   return done(err);
+        // }
+        if (!user) {
+          return done(null, false, { msg: `Email ${email} not found.` });
         }
-        return done(null, false, { msg: 'Invalid email or password.' })
-      })
+        if (!user.password) {
+          return done(null, false, {
+            msg:
+              "Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.",
+          });
+        }
+        user.comparePassword(password, (err, isMatch) => {
+          if (err) {
+            return done(err);
+          }
+          if (isMatch) {
+            return done(null, user);
+          }
+          return done(null, false, { msg: "Invalid email or password." });
+        });
+      // });
     })
-  }))
-  
+  );
 
   passport.serializeUser((user, done) => {
-    done(null, user.id)
-  })
+    done(null, user.id);
+  });
 
   passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user))
-  })
-}
+
+    User.findById(id) //when we want the user to come out of the session, we will grab that user id that was store there and find it in the database
+    .then((user)=>{
+      done(null,user);
+    })
+    .catch(function (err) {
+      console.log(err);
+    })
+  });
+};
+
+
+//   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+//     User.findOne({ email: email.toLowerCase() }, (err, user) => {
+//       if (err) { return done(err) }
+//       if (!user) {
+//         return done(null, false, { msg: `Email ${email} not found.` })
+//       }
+//       if (!user.password) {
+//         return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' })
+//       }
+//       user.comparePassword(password, (err, isMatch) => {
+//         if (err) { return done(err) }
+//         if (isMatch) {
+//           return done(null, user)
+//         }
+//         return done(null, false, { msg: 'Invalid email or password.' })
+//       })
+//     })
+//   }))
+  
+
+//   passport.serializeUser((user, done) => {
+//     done(null, user.id)
+//   })
+
+//   passport.deserializeUser((id, done) => {
+//     User.findById(id, (err, user) => done(err, user))
+//   })
+// }

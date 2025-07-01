b32189e0a28016862fee6f8f240ea57022fd6135
pini91 <brenda.loncaric@gmail.com>
2025-07-01 11:52:18 -0400

    first commit

---
+++ routes/todos.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const todosController = require('../controllers/todos') 
+const { ensureAuth } = require('../middleware/auth')
+
+router.get('/', ensureAuth, todosController.getTodos)
+
+router.post('/createTodo', todosController.createTodo)
+
+router.put('/markComplete', todosController.markComplete)
+
+router.put('/markIncomplete', todosController.markIncomplete)
+
+router.delete('/deleteTodo', todosController.deleteTodo)
+
+module.exports = router
+//module.exports:we can think of like functions ready to be called or objects ready to be used until they are required.
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('express-flash')
+const logger = require('morgan')
+const connectDB = require('./config/database') // here I require the function from the config... from the module.export = connectDB
+const mainRoutes = require('./routes/main')
+const todoRoutes = require('./routes/todos')
+
+require('dotenv').config({path: './config/.env'}) // for our environment variables.
+
+// Passport config
+require('./config/passport')(passport)
+
+connectDB()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))//line1..this 2 lines replace something called body parser.
+app.use(express.json())//line2.. meaning we can pars stuff that comes from the body. not just stuff from the form. But stuff we are trying to click.
+app.use(logger('dev'))
+// Sessions
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  )
+  
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
+  
+app.use('/', mainRoutes)
+app.use('/todos', todoRoutes)
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log('Server is running, you better catch it!')
+})    
